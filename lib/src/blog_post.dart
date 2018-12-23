adbea8c0c24c68cb98ac7dcfefc1aa1cff5c1990
JohanBaskovec <johan@johanbaskovec.fr>
2018-12-23 03:13:21 +0100

    Add linter and analyzer rules

--- lib/src/blog_post.dart
+++ lib/src/blog_post.dart
@@ -2,12 +2,21 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'blog_post.g.dart';
 
+/// A blog post
 @JsonSerializable(nullable: false)
 class BlogPost {
+  /// The title of the post.
   String title;
+  /// The content of the post, in HTML.
   String content;
 
+  /// Creates a new [BlogPost].
   BlogPost({this.title, this.content});
-  factory BlogPost.fromJson(Map<String, dynamic> json) => _$BlogPostFromJson(json);
+
+  /// Creates a new [BlogPost] from a JSON object.
+  static BlogPost fromJson(Map<String, dynamic> json) =>
+      _$BlogPostFromJson(json);
+
+  /// Convert this object to a JSON object.
   Map<String, dynamic> toJson() => _$BlogPostToJson(this);
 }
--- test/blog_post_test.dart
+++ test/blog_post_test.dart
@@ -4,28 +4,29 @@ import 'package:test/test.dart';
 void main() {
   group('constructor', () {
     test('should work', () {
-      var blogPost = BlogPost(title: "title", content: "content");
-      expect(blogPost.title, equals("title"));
-      expect(blogPost.content, equals("content"));
+      final BlogPost blogPost = BlogPost(title: 'title', content: 'content');
+      expect(blogPost.title, equals('title'));
+      expect(blogPost.content, equals('content'));
     });
   });
   group('fromJson', () {
     test('should create instance from json', () {
-      Map<String, dynamic> json = {
-        "title": "test-title",
-        "content": "test-content"
+      final Map<String, dynamic> json = {
+        'title': 'test-title',
+        'content': 'test-content'
       };
-      var blogPost = BlogPost.fromJson(json);
-      expect(blogPost.content, equals(json["content"]));
-      expect(blogPost.title, equals(json["title"]));
+      final BlogPost blogPost = BlogPost.fromJson(json);
+      expect(blogPost.content, equals(json['content']));
+      expect(blogPost.title, equals(json['title']));
     });
   });
   group('toJson', () {
     test('should create map from object', () {
-      var blogPost = BlogPost(title: "test-title", content: "test-content");
-      var json = blogPost.toJson();
-      expect(json["content"], equals(blogPost.content));
-      expect(json["title"], equals(blogPost.title));
+      final BlogPost blogPost =
+          BlogPost(title: 'test-title', content: 'test-content');
+      final Map<String, dynamic> json = blogPost.toJson();
+      expect(json['content'], equals(blogPost.content));
+      expect(json['title'], equals(blogPost.title));
     });
   });
 }
